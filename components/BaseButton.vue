f4edc8a2ab567a0d1a3f22c1a64579cfe2b18b35
RubenMMPS <rmartinmotilla@gmail.com>
2026-03-13 12:39:07 +0100

    feat: add BaseButton component with dynamic styling

---
+++ components/BaseButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <button
+    :class="[
+      'py-2 px-4 rounded hover:bg-brand-secondary',
+      type === 'primary'
+        ? 'bg-brand-primary'
+        : 'border border-white text-white',
+    ]"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  text: string;
+  type: "primary" | "secondary";
+}>();
+</script>
