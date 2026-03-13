89183950d5f122cdf0c735c01a7994edac584dc5
RubenMMPS <rmartinmotilla@gmail.com>
2026-03-13 12:44:21 +0100

    feat: add and implement AppHeader and AppFooter components for layout structure

---
+++ components/AppFooter.vue
@@ -0,0 +1,25 @@
+<script setup>
+const icons = {
+  infojobs: { link: "streamline-logos:linkedin-logo-block", color: "white" },
+  github: { link: "streamline-logos:github-logo-2-solid", color: "white" },
+  twitter: { link: "streamline-logos:twitter-logo-1-solid", color: "white" },
+};
+</script>
+
+<template>
+  <footer
+    class="flex flex-col md:flex-row items-center p-4 bg-header-footer shadow mt-8 text-white"
+  >
+    <p class="md:mr-auto">@ 2026 Rubén Built with Tailwind CSS</p>
+    <div class="md:ml-auto flex items-center gap-4">
+      <div v-for="icon in icons" :key="icon.link">
+        <Icon
+          :name="icon.link"
+          size="30px"
+          :style="{ color: icon.color }"
+          class="ml-2 mr-2"
+        />
+      </div>
+    </div>
+  </footer>
+</template>
---
+++ components/AppHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+    <nav class="flex items-center gap-4 p-4 bg-header-footer shadow text-white">
+      <NuxtLink to="/home"><h2>Portfolio</h2></NuxtLink>
+      <div class="ml-auto flex items-center gap-4">
+        <NuxtLink to="#baseCard" class="btn">Home</NuxtLink>
+        <NuxtLink to="#about" class="btn">About</NuxtLink>
+        <NuxtLink to="#skills" class="btn">Skills</NuxtLink>
+        <NuxtLink to="#proyects" class="btn">Projects</NuxtLink>
+        <NuxtLink to="#experience" class="btn">Experience</NuxtLink>
+      </div>
+    </nav>
+  </header>
+</template>
